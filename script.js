caa31b3564e85ab8778f7243a8677fd31100209d
Stephany1703 <stephany.rosa@aluno.ifsp.edu.br>
2024-10-30 12:55:06 +0000

    questão

---
+++ script.js
@@ -0,0 +1,8 @@
+let aluno = "João da Silva";
+let nota1 = 7;
+let nota2 = 8.5;
+let nota_06 = nota1 * 0.6;
+let nota_04 = nota2 * 0.4;
+let nota_final = nota_06 + nota_04
+alert("Aluno: " + aluno + "\nNota 1: " + nota1 + "\nNota 2: " + nota2);
+alert("Aluno: " + aluno+ "\nNota Final: " + nota_final);
